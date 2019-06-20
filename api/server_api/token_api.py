166b0e8dbf4bc374c7e36090a79e959435d992a9
zhouxu <18333040388@163.com>
2019-06-20 16:16:39 +0800

    Create api, create api of take token

---
+++ api/server_api/token_api.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+from flask.ext.restful import reqparse, abort, Api, Resource
+
+token_api = Blueprint('token', __name__)
+
+api = Api(token)
+
+class Todo(Resource):
+    def get(self):
+        return {'todo': 'test'}
+
+api.add_resource(Todo, '/todo')
