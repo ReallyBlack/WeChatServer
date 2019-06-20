36e142b1005aaea8ae467def706e8a3ae14aa0bb
ReallyBlack <18333040388@163.com>
2019-06-20 17:46:25 +0800

    update token_api, test it and success

--- api/server_api/token_api.py
+++ api/server_api/token_api.py
@@ -3,10 +3,12 @@ from flask_restful import reqparse, abort, Api, Resource
 
 token_api = Blueprint('token', __name__)
 
-api = Api(token)
+api = Api(token_api)
 
 class Todo(Resource):
     def get(self):
         return {'todo': 'test'}
+    def post(self):
+        return {'todo': 'post test'}
 
 api.add_resource(Todo, '/todo')
