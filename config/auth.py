f28152f9796a3e48d610523e1f084d806c0fca8a
zhouxu <18333040388@163.com>
2019-06-20 09:12:20 +0800

    Update auth, without new power

--- config/auth.py
+++ config/auth.py
@@ -1,4 +1,17 @@
+"""
+in the file, you can write what you will use in the server globle.
+"""
+
+
+
+# write you self app info to the dict
+# if you have more keys you can add it by yourself
+# the must key : token, appID, appsecret
+# please don't change the above keys name 
 APPINFO = {
+    # "token": "your own token",
+    # "appID": "The ID of your weixin app ID",
+    # "appsecret": "your appsecret, you can read office document and got it"
     "token": 'Aidnd253@%#jdif*76',
     "appID": 'wxbc14d72905753ea1',
     "appsecret": '261f5cabd65e88cfce767fffb65d34fc'
