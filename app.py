1bae60e61a27c8a4fe01c9a2a9b2ab103faec4a4
zhouxu <18333040388@163.com>
2019-06-20 09:11:28 +0800

    Update app, write some new mind in the file

--- app.py
+++ app.py
@@ -8,14 +8,21 @@ app = Flask(__name__)
 
 @app.route('/')
 @verify.isServer
-def index(echostr):
+def index(echostr, code):
     if request.method == 'GET':
         # todo:返回给服务器特定的响应认证
-        print(echostr)
-        return "code:200"
+        if code:
+            return echostr
+        else:
+            return echostr
     elif request.method == 'POST':
         # todo:根据响应类型进行特定的响应处理
-        return "code:202"
+        # 1.验证消息来源，如果是来自微信服务器，则对数据进行处理
+        if code:
+            # todo:
+
+        # 2.如果消息不是来自微信服务器，则返回错误码
+        return "code: 503"
     else:
         # todo:反正错误响应代码
         return "code:404"
