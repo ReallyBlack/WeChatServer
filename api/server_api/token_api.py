0627c0e039966191330b5e484b8eea301911b0bb
zhouxu <18333040388@163.com>
2019-06-21 12:25:12 +0800

    Update token_api nothing be change

--- api/server_api/token_api.py
+++ api/server_api/token_api.py
@@ -1,12 +1,9 @@
-import requests
-import redis
+import requests, redis, xmltodict
 from flask import Blueprint
 from flask_restful import reqparse, abort, Api, Resource
 
 import time
 import ast
-import xmltodict
-
 from config import auth
 
 appinfo = auth.APPINFO
@@ -15,8 +12,8 @@ appinfo = auth.APPINFO
 # sql = settings.get('server_sql')
 
 token_api = Blueprint('token', __name__)
-
 api = Api(token_api)
+
 connect_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, db=2)
 # redis_cli = redis.StrRedis(connection_pool=connect_pool, decode_responses=True)
 
@@ -48,12 +45,13 @@ def save_db(key):
     else:
         field = {"t": False}
     try:
-        redis_cli = redis.StrictRedis(connection_pool=connect_pool, decode_responses=True)
+        redis_cli = redis.StrictRedis(connection_pool=connect_pool)
         redis_cli.hmset(key, field)
         return field.get("t", False)
     except:
         False
 
+# 从redis中读取已经存入的数据
 def read_db(key):
     try:
         redis_cli = redis.StrictRedis(connection_pool=connect_pool)
@@ -79,13 +77,3 @@ class Token(Resource):
                 return {"message": "no token"},200
 
 api.add_resource(Token, '/t/<string:key>')
-
-"""
-class Todo(Resource):
-    def get(self):
-        return {'todo': 'test'}
-    def post(self):
-        return {'todo': 'post test'}
-
-api.add_resource(Todo, '/todo')
-"""
