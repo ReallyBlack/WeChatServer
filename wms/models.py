31105700dc3047624acd4cdd8221740cc4950acb
ReallyBlack <18333040388@163.com>
2019-06-21 20:17:31 +0800

    update models.py

--- wms/models.py
+++ wms/models.py
@@ -1,8 +1,17 @@
 from WeChatServer.tools.db import db
 
 
-class admin(db.Model):
-    __tablename__ = 'admin'
+class admin_list(db.Model):
+    __tablename__ = 'admin_list'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
+    id_code = db.Column(db.String(100), primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    salt_password = db.Column(db.String(100), nullable=False)
+    phone_num = db.Column(db.String(11), nullable=False)
+    wechat = db.Column(db.String(20))
+    nick_name = db.Column(db.String(20))
+    permission_user = db.Column(db.Boolean, default=False)
+    permission_message = db.Column(db.Boolean, default=False)
+    permission_article = db.Column(db.Boolean, default=False)
+    is_root = db.Column(db.Boolean, default=False)
+    toekn = db.Column(db.String(100))
