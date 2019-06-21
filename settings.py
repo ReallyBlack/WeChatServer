2922095aa30610425e0a25246d949cd60c1d8b36
zhouxu <18333040388@163.com>
2019-06-21 15:20:43 +0800

    Create settings write default config of db

---
+++ settings.py
@@ -0,0 +1,16 @@
+from config import settings
+
+SQL = settings.SQL.get('back_sql')
+
+
+class config():
+    SECRET_KEY = "saldifwnk23425@!#!kfa"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = '{}+pymysql://{}:{}@{}:{}/{}'.format(
+        SQL.get('sql').lower(),
+        SQL.get('user'),
+        SQL.get('password'),
+        SQL.get('host'),
+        SQL.get('port'),
+        SQL.get('db')
+    )
