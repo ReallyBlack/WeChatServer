c2ddbefc890614a60962f88e986f78ec7ec6083c
ReallyBlack <18333040388@163.com>
2019-06-22 12:57:59 +0800

    update 将后台系统接口注册到app

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from .settings import config
 
 from .tools import db
 from .api.server_api import token_api
+from WeChatServer.api.back_api import admin_api
 from .wms import wms
 
 def create_app():
@@ -10,9 +11,11 @@ def create_app():
     # 配置数据库信息
     app.config.from_object(config)
 
-    # 注册蓝图信息
-    app.register_blueprint(token_api, url_prefix='/api/server/api')
-    app.register_blueprint(wms, url_prefix='/wms')
+    # 注册蓝图及api
+    app.register_blueprint(token_api, url_prefix='/api/server/api')  # 服务器端访问接口
+    app.register_blueprint(wms, url_prefix='/wms')  # 后台系统蓝图
+    app.register_blueprint(admin_api, url_prefix='/api/back/api')  # 后台系统访问接口
+
 
     db.init_app(app)
     with app.app_context():
