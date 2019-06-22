e5b7759d0a1af4179615271fa6d1418b70458fb5
ReallyBlack <18333040388@163.com>
2019-06-22 17:40:20 +0800

    nothing

--- wms/views.py
+++ wms/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
-from flask import render_template
+from flask import render_template, request
 
 views_bp = Blueprint('wms', __name__)
 
 
 @views_bp.route('/')
 def index():
-    context = { "user": 'zhouxu'}
+    if request.headers.get('token', None) is None:
+    context = { ss"user": 'zhouxu'}
     return render_template('index.html', context=context)
