d7821c79f1cf343d4663bcee6c9ba8768ede97ec
ReallyBlack <18333040388@163.com>
2019-07-05 18:15:38 +0800

    声明密码验证的装饰器

--- tools/verify.py
+++ tools/verify.py
@@ -24,3 +24,10 @@ def isServer(f):
             is_server = False
         return f(echostr=verifycode, code=is_server)
     return wrapper
+
+
+def verify_password(func):
+    @wraps(func)
+    def wrapper():
+        return func()
+    return wrapper
