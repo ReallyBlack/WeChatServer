4a287addde7efde058880c1462f71d8d39e3b3e2
ReallyBlack <18333040388@163.com>
2019-07-07 17:54:13 +0800

    更新了block不正确的bug

--- application/models.py
+++ application/models.py
@@ -24,25 +24,25 @@ class fancy_list(db.Model):
     openid = db.Column(db.String(100), primary_key=True)
     subscribe = db.Column(db.String(1))  # 订阅标识，0为未关注
     nickname = db.Column(db.String(100))  # 用户的昵称
-	sex = db.Column(db.String(1))  # 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
-	city = db.Column(db.String(20))  # 用户所在城市
-	country = db.Column(db.String(20))  # 用户所在国家
-	province = db.Column(db.String(20))  # 用户所在省份
-	language = db.Column(db.String(20))  # 用户的语言，简体中文为zh_CN
-	headimgurl = db.Column(db.String(200))  
-        # 用户头像，最后一个数值代表正方形头像大小
-        #（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。
-        # 若用户更换头像，原有头像URL将失效。
-	subscribe_time = db.Column(db.String(50))  # 用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
-	unionid = db.Column(db.String(100))  # 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
-	remark = db.Column(db.String(50))  # 公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注
-	groupid = db.Column(db.String(10))  # 用户所在的分组ID（兼容旧的用户分组接口）
-	tagid_list = db.Column(db.String(500))  # 用户被打上的标签ID列表
-	subscribe_scene = db.Column(db.String(50))  
-        # 返回用户关注的渠道来源，ADD_SCENE_SEARCH 公众号搜索，
-        # ADD_SCENE_ACCOUNT_MIGRATION 公众号迁移，ADD_SCENE_PROFILE_CARD 名片分享，
-        # ADD_SCENE_QR_CODE 扫描二维码，ADD_SCENEPROFILE LINK 图文页内名称点击，
-        # ADD_SCENE_PROFILE_ITEM 图文页右上角菜单，ADD_SCENE_PAID 支付后关注，
-        # ADD_SCENE_OTHERS 其他
-	qr_scene = db.Column(db.String(50))  # 二维码扫码场景（开发者自定义）
-	qr_scene_str = db.Column(db.String(50))  # 二维码扫码场景描述（开发者自定义）
+    sex = db.Column(db.String(1))  # 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+    city = db.Column(db.String(20))  # 用户所在城市
+    country = db.Column(db.String(20))  # 用户所在国家
+    province = db.Column(db.String(20))  # 用户所在省份
+    language = db.Column(db.String(20))  # 用户的语言，简体中文为zh_CN
+    headimgurl = db.Column(db.String(200))  
+    # 用户头像，最后一个数值代表正方形头像大小
+    #（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。
+    # 若用户更换头像，原有头像URL将失效。
+    subscribe_time = db.Column(db.String(50))  # 用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
+    unionid = db.Column(db.String(100))  # 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
+    remark = db.Column(db.String(50))  # 公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注
+    groupid = db.Column(db.String(10))  # 用户所在的分组ID（兼容旧的用户分组接口）
+    tagid_list = db.Column(db.String(500))  # 用户被打上的标签ID列表
+    subscribe_scene = db.Column(db.String(50))  
+    # 返回用户关注的渠道来源，ADD_SCENE_SEARCH 公众号搜索，
+    # ADD_SCENE_ACCOUNT_MIGRATION 公众号迁移，ADD_SCENE_PROFILE_CARD 名片分享，
+    # ADD_SCENE_QR_CODE 扫描二维码，ADD_SCENEPROFILE LINK 图文页内名称点击，
+    # ADD_SCENE_PROFILE_ITEM 图文页右上角菜单，ADD_SCENE_PAID 支付后关注，
+    # ADD_SCENE_OTHERS 其他
+    qr_scene = db.Column(db.String(50))  # 二维码扫码场景（开发者自定义）
+    qr_scene_str = db.Column(db.String(50))  # 二维码扫码场景描述（开发者自定义）
